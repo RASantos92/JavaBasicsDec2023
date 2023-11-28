bffced56a16d21298a47618fd322502906200479
RASantos92 <rasbms81608@gmail.com>
2023-11-28 15:51:58 -0600

    11/28 Static member variables

--- Tuesday/PM/Dev3/Developer.java
+++ Tuesday/PM/Dev3/Developer.java
@@ -1,24 +1,38 @@
 import java.util.ArrayList;
 public class Developer{
+    //------------------ attributes (private/protected) ----------------------
+    private static int devCount;
+    private static double totalSalary;
     private String name;
     private int age;
     private double salary;
     private ArrayList<String> languages = new ArrayList<String>();
     private ArrayList<Project> projects = new ArrayList<Project>();
 
-    public Developer(){}
+
+    //------------------ constructors ----------------------
+    public Developer(){
+        Developer.devCount++;
+    }
 
     public Developer(int age, double salary){
+
         this.name = "no name";
         this.age = age;
         this.salary = salary;
+        Developer.totalSalary += salary;
+        Developer.devCount++;
     }
+
 
     public Developer(String name, int age, double salary){
         this.name = name;
         this.age = age; 
         this.salary = salary;
+        Developer.totalSalary += salary;
+        Developer.devCount++;
     }
+    //------------------ other class methods ----------------------
 
     public void displayStatus(){
         System.out.println("name: " + this.name);
@@ -35,6 +49,13 @@ public class Developer{
         this.projects.add(project);
     }
 
+
+    public void addLanguage(String language){
+        this.languages.add(language);
+    }
+
+
+    //------------------ getters / setters  ----------------------
     public String getName(){
         return this.name;
     }
@@ -55,12 +76,17 @@ public class Developer{
         return this.salary;
     }
 
-    public void setSalary(double salary){
+    public void setSalary(double salary){ // 400$ =>
+        Developer.totalSalary += (salary - this.salary);
         this.salary = salary;
     }
 
-    public void addLanguage(String language){
-        this.languages.add(language);
+    public static int getDevCount(){
+        return Developer.devCount;
+    }
+
+    public static double getTotalSalary(){
+        return Developer.totalSalary;
     }
 
 }
